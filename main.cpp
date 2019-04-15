04341a78fc384ecf604a1b97a205a178fea4532d
LucasSsouza10 <luccas_ssouza@hotmail.com>
2019-04-15 20:46:43 -0300

    primeiro commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 15 de abr de 2019
+ *      Author: lucca
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<< "Hello World!" << endl;
+	return 0;
+}
+
