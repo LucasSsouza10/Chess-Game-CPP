d6a6334102ce9d3169e44d782bfec0a3c722726a
walo2 <wilson.al.oliver@gmail.com>
2019-04-15 22:40:01 -0300

    Declaração dos atributos e métodos da classe Jogo

--- Jogo.h
+++ Jogo.h
@@ -8,7 +8,21 @@
 #ifndef JOGO_H_
 #define JOGO_H_
 
+#include "Tabuleiro.h"
+#include "Jogador.h"
+#include "Peca.h"
+
 class Jogo {
+private:
+	Tabuleiro tab;
+	Jogador j1, j2;
+	Peca p[24];
+	int estado; //0 = início do jogo; 1 = xeque; 2 = xeque-mate
+	int vez; //0 = jogador 1; 1 = jogador 2
+public:
+	Jogo(); //Construtor padrão
+	~Jogo(); //Destrutor
+
 };
 
 #endif /* JOGO_H_ */
