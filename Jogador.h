0f837f4d001d78fbb81a2f3bdbdf3515901bd4c7
LucasSsouza10 <luccas_ssouza@hotmail.com>
2019-04-15 22:00:33 -0300

    Criação dos header Jogadoe e Jogo

---
+++ Jogador.h
@@ -0,0 +1,14 @@
+/*
+ * Jogador.h
+ *
+ *  Created on: 15 de abr de 2019
+ *      Author: lucca
+ */
+
+#ifndef JOGADOR_H_
+#define JOGADOR_H_
+
+class Jogador {
+};
+
+#endif /* JOGADOR_H_ */
---
+++ Jogo.h
@@ -0,0 +1,14 @@
+/*
+ * Jogo.h
+ *
+ *  Created on: 15 de abr de 2019
+ *      Author: lucca
+ */
+
+#ifndef JOGO_H_
+#define JOGO_H_
+
+class Jogo {
+};
+
+#endif /* JOGO_H_ */
