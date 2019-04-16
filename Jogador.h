8a6fbdaa904d8e8239c36f7a6e077ed2e6828658
LucasSsouza10 <luccas_ssouza@hotmail.com>
2019-04-15 22:36:59 -0300

    Implementação Jogador.h

--- Jogador.h
+++ Jogador.h
@@ -8,7 +8,17 @@
 #ifndef JOGADOR_H_
 #define JOGADOR_H_
 
+#include <iostream>
+
+#include <Peca.h>
+
 class Jogador {
+	private:
+		std::string nome;
+		std::string cor;
+		Peca *pecas;
+	public:
+		Jogador(std::string cor);
 };
 
 #endif /* JOGADOR_H_ */
