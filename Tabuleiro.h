6b0ce11cb8de9b6624dec1bbd7bf7f7f1ab855f5
LucasSsouza10 <luccas_ssouza@hotmail.com>
2019-04-17 13:25:51 -0300

    Definição header tabuleiro

--- Tabuleiro.h
+++ Tabuleiro.h
@@ -1,14 +1,17 @@
-/*
- * Tabuleiro.h
- *
- *  Created on: 15 de abr de 2019
- *      Author: lucca
- */
 
 #ifndef TABULEIRO_H_
 #define TABULEIRO_H_
 
+#include "Posicao.h";
+
 class Tabuleiro {
+private:
+	Posicao pos[8][8];
+public:
+	Tabuleiro();	//responsavel pela configuração inicial do tabuleiro
+	void movimenta(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino); // responsavel pela manutenção e checagem do tabuleiro a cada jogada
+	void desenharTabuleiro(); //desenha o tabuleiro na tela
+
 };
 
 #endif /* TABULEIRO_H_ */
