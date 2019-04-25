f14ecd4fd6fea612458aa85f7d42be1e6ba315eb
matheusaraujo789 <matheusliber1@hotmail.com>
2019-04-25 16:47:48 -0300

    Contrutores

--- Tabuleiro.cpp
+++ Tabuleiro.cpp
@@ -6,4 +6,32 @@
  */
 
 #include "Tabuleiro.h"
+Tabuleiro::Tabuleiro(Peca *p1, Peca *p2)
+{
+	int aux=0;// auxiliar na hora de preencher as posicoess
+//0 a 7 primeira fila
+//8 a 15 segunda fila
+	for(int i=0;i<2;i++) //linha
+	{
+		if(i==1)
+			aux=7;
+		for (int j=0;j<8;j++)
+			pos[i][j].setPca(p1[j+aux]) // a intencao aqui e passar o endereco de uma peca vindo do conjunto do jogo;
 
+	}
+	for(int i=2;i<6;i++) //linha
+		{
+			for (int j=0;j<8;j++)
+				pos[i][j].setPca(NULL) // a intencao aqui e passar o endereco de uma peca vindo do conjunto do jogo;
+
+		}
+
+	for(int i=6;i<8;i++) //linha
+		{
+			if(i==7)
+				aux=7;
+			for (int j=0;j<8;j++)
+				pos[i][j].setPca(p2[j+aux]) // a intencao aqui e passar o endereco de uma peca vindo do conjunto do jogo;
+
+		}
+}
