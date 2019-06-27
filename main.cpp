3bf418a9b8085420d983de07eecbba8cf373f163
LucasSsouza10 <luccas_ssouza@hotmail.com>
2019-06-27 16:35:22 -0300

    jogo com nome e sem

--- main.cpp
+++ main.cpp
@@ -1,44 +1,74 @@
 #include <iostream>
 #include "Jogo.h"
-#include "Posicao.h"
-#include "Dama.h"
-#include "Torre.h"
-#include "Rei.h"
-#include "Cavalo.h"
-#include "Bispo.h"
 
 using namespace std;
 
 int main() {
 	cout << "---------- XADREZ ----------" << endl;
+	char n;
+	cout << "Deseja inserir nome? S/N" << endl;
+	cin >> n;
+	n = toupper(n);
 
-	string j1, j2;
-	cout << "Jogador 1, por favor, digite seu nome: " << endl;
-	cin >> j1;
-	cout << "Jogador 2, por favor, digite seu nome:" << endl;
-	cin >> j2;
+	while (n != 'N' && n != 'S') {
+		cout << "Entrada invalida! Digite: (S/N)";
+		cin >> n;
+		n = toupper(n);
+	}
 
-	Jogo jogo(j1, j2);
+	if (n == 'S') {
+		string j1, j2;
+		cout << "Jogador 1, por favor, digite seu nome: " << endl;
+		cin >> j1;
+		cout << "Jogador 2, por favor, digite seu nome:" << endl;
+		cin >> j2;
+		Jogo jogo(j1, j2);
 
-	char op;
-	cout << "Deseja retornar ao último ponto de parada? (S/N): ";
-	cin >> op;
-	op = toupper(op);
-	while (op != 'N' && op != 'S') {
-		cout << "Entrada invalida! Digite: S/N: ";
+		char op;
+		cout << "Deseja retornar ao último ponto de parada? (S/N)";
 		cin >> op;
 		op = toupper(op);
-	}
+		while (op != 'N' && op != 'S') {
+			cout << "Entrada invalida! Digite: S/N: ";
+			cin >> op;
+			op = toupper(op);
+		}
 
-	if (op == 'S') {
-        cout << "Carregando o jogo..." << endl;
-		jogo.carregar(); //carrega jogo salvo
+		if (op == 'S') {
+			cout << "Carregando o jogo..." << endl;
+			jogo.carregar(); //carrega jogo salvo
+		} else {
+			jogo.limparSalvo(); //começa um novo jogo
+		}
+
+		cin.ignore();
+		jogo.playGame();
+		cout << "Jogo finalizado." << endl;
+
 	} else {
-		jogo.limparSalvo(); //começa um novo jogo
-	}
 
-	cin.ignore();
-	jogo.playGame();
-	cout << "Jogo finalizado." << endl;
+		Jogo jogo;
+
+		char op;
+		cout << "Deseja retornar ao último ponto de parada? (S/N)";
+		cin >> op;
+		op = toupper(op);
+		while (op != 'N' && op != 'S') {
+			cout << "Entrada invalida! Digite: S/N: ";
+			cin >> op;
+			op = toupper(op);
+		}
+
+		if (op == 'S') {
+			cout << "Carregando o jogo..." << endl;
+			jogo.carregar(); //carrega jogo salvo
+		} else {
+			jogo.limparSalvo(); //começa um novo jogo
+		}
+
+		cin.ignore();
+		jogo.playGame();
+		cout << "Jogo finalizado." << endl;
+	}
 }
 
